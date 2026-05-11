af9ee46e310b32736e2dd8b9effefbfb5c1eed80
JuanICasareski <juani.casareski@gmail.com>
2026-05-10 22:27:58 -0300

    feat: push changes to db on pr workflow

--- packages/infra/modules/secrets.ts
+++ packages/infra/modules/secrets.ts
@@ -1,3 +1,4 @@
 export const jwtSecret = new sst.Secret("JwtSecret");
+export const dbId = new sst.Secret("DbId", "pending");
 
-export const secrets = [jwtSecret];
+export const secrets = [jwtSecret, dbId];
