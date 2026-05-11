995e3aaaa93c764afde7aabe89aa1c6ba051c1a6
JuanICasareski <juani.casareski@gmail.com>
2026-05-10 23:01:01 -0300

    fix: add WEB_ORIGIN for CORS on sst deploy script

--- packages/infra/modules/api.ts
+++ packages/infra/modules/api.ts
@@ -1,13 +1,16 @@
 import { auctionDurableObject } from "./auction";
 import { db } from "./database";
 import { secrets } from "./secrets";
-import { apiDomain, stage } from "./stage";
+import { apiDomain, stage, webDomain } from "./stage";
 
 export const api = new sst.cloudflare.Worker("Api", {
   handler: "../../apps/api/src/index.ts",
   url: true,
   domain: apiDomain,
   link: [db, ...secrets],
+  environment: {
+    WEB_ORIGIN: `https://${webDomain}`,
+  },
   transform: {
     worker: workerArgs => {
       workerArgs.compatibilityDate = "2026-04-02";
