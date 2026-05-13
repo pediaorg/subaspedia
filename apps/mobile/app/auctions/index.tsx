58376ed9421897df8bb1f554b9a119730020ccd2
Santiago Ribecca <ribeccasantiago@gmail.com>
2026-05-13 13:42:21 -0300

    style: enhance AuctionsScreen layout and button styles for better UX

--- apps/mobile/app/auctions/index.tsx
+++ apps/mobile/app/auctions/index.tsx
@@ -48,13 +48,13 @@ export default function AuctionsScreen() {
           {AUCTIONS.map(auction => (
             <View
               key={auction.id}
-              className="h-48 w-full mt-3 mb-15 drop-shadow-xl/50"
+              className="w-full mt-3 mb-5 drop-shadow-xl/50 hover: duration-300 focus:drop-shadow-xl/80 active:drop-shadow-xl/80 rounded-lg bg-white transition delay-150 duration-300 ease-in-out hover:-translate-y-1 focus:-translate-y-1 active:-translate-y-1"
             >
               <Image
                 source={{ uri: "https://placehold.co/800x600" }}
-                className="w-full h-full rounded-t-lg"
+                className="w-full h-40 rounded-t-lg"
               />
-              <View className="h-12 bg-primary flex flex-row justify-between items-center rounded-b-lg px-4">
+              <View className="h-12 bg-primary flex flex-row justify-between items-center rounded-b-lg px-4 drop-shadow-top-lg/50">
                 <View className="p-1">
                   <Text className="text-white font-bold">Catálogo</Text>
                   <View className="flex flex-row">
@@ -62,14 +62,17 @@ export default function AuctionsScreen() {
                       <Image
                         key={uri}
                         source={{ uri }}
-                        className={`w-6 h-6 rounded-full border border-primary ${
+                        className={`w-6 h-6 rounded-full border border-primary drop-shadow-lg ${
                           index === 0 ? "" : "-ml-2"
                         }`}
                       />
                     ))}
                   </View>
                 </View>
-                <Button className="border-border h-8 bg-white rounded-full">
+                <Button
+                  className="border-border h-8 bg-white rounded-full drop-shadow-lg hover:bg-white/90 active:bg-white/90 focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-primary/80"
+                  size="sm"
+                >
                   Entrar
                 </Button>
               </View>
