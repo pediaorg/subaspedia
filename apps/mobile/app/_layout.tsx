65016ef31f9b4cdb3c97e86d5240b0b508879a0e
Santiago Ferreyra <santiagoferreyra301@gmail.com>
2026-06-22 10:36:32 -0300

    feat: initialize root layout with providers, navigation stack, and global components

--- apps/mobile/app/_layout.tsx
+++ apps/mobile/app/_layout.tsx
@@ -31,6 +31,10 @@ export default function RootLayout() {
             />
             <Stack.Screen name="register" options={{ headerShown: false }} />
             <Stack.Screen name="logout" options={{ headerShown: false }} />
+            <Stack.Screen
+              name="insurances/index"
+              options={{ headerShown: false }}
+            />
           </Stack>
 
           <BottomNav />
